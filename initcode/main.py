035f5f2086e5a63c842bae1eec1dcec7aafac544
TJ-DA1 <terrygannon64@gmail.com>
2025-10-15 16:40:54 +0100

    the initialisation of the universe

---
+++ initcode/main.py
@@ -0,0 +1,13 @@
+class __init__:
+    __init__ = "__init__"
+    def __init__(self, __init__) -> type(__init__):
+        print("__init__", __init__.__init__)
+        self.__init__ = __init__.__init__()
+
+class __init__(__init__):
+    def __init__(self,__init__ = __init__) -> __init__:
+        __init__(__init__.__init__).__init__
+        __init__(self.__init__ == __init__(self.__init__))
+    def __init__(self) -> __init__(__init__, __init__ = __init__):
+        __init__ = self.__init__.__init__
+__init__()
